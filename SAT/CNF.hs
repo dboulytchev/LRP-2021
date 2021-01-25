ea1c0a11bd08bfb282827137c1b7ae85f8871d5f
Alexey Trilis <aatrilis2000@gmail.com>
2021-01-25 20:09:11 +0300

    hw01

--- SAT/CNF.hs
+++ SAT/CNF.hs
@@ -7,13 +7,38 @@ module CNF where
 import Formula
 import Data.List
 import Control.Monad.State.Lazy
+import Test.QuickCheck
 
 -- A type for CNF: a list (conjunction) of lists (clauses) of
 -- literas (positive or negative variable names)
 type CNF = [[Var]]
 
+toList :: F -> CNF
+toList (a :/\: b) = toList a ++ toList b 
+toList c@(a :\/: b) = [toListC c] where
+  toListC (a :\/: b) = toListC a ++ toListC b
+  toListC (Var v) = [v]
+  toListC (Neg (Var v)) = [-v]
+toList (Var v) = [[v]]
+toList (Neg (Var v)) = [[-v]]
+
+tseytin :: F -> Int -> (CNF, Var, Int)
+tseytin f maxVar = case f of
+  (Var v) -> ([], v, maxVar)
+  (Neg (Var v)) -> ([], -v, maxVar)
+  (a :/\: b) -> helper (\ x y -> x :/\: y) a b
+  (a :\/: b) -> helper (\ x y -> x :\/: y) a b
+  (a :<=>: b) -> helper (\ x y -> x :<=>: y) a b
+  (a :=>: b) -> helper (\ x y -> x :=>: y) a b
+  (Neg a) -> let (res, x, maxVar') = tseytin a maxVar in (res, -x, maxVar')
+  where helper op a b = let (res1, x, maxVar') = tseytin a maxVar in 
+                        let (res2, y, maxVar'') = tseytin b maxVar' in 
+                        let v = maxVar'' + 1 in
+                        ((toList $ Formula.toCNF $ (Var v) :<=>: (op (Var x) (Var y))) ++ res1 ++ res2, v, v)
+
 -- Tseitin's conversion
-toCNF f = undefined
+toCNF :: F -> CNF
+toCNF f = let (x, v, _) = tseytin f (maxVar f) in ([v]):x
 
 -- The inverse conversion
 fromCNF (h : tl) = foldl (\ acc c -> acc :/\: fromClause c) (fromClause h) tl where
