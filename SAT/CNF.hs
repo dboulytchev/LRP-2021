f0f16322314dc2c1d776120e7806a85288adfd8b
Your Name <you@example.com>
2021-01-27 15:50:03 +0300

    Finish hw01

--- SAT/CNF.hs
+++ SAT/CNF.hs
@@ -8,14 +8,47 @@ import Formula
 import Data.List
 import Control.Monad.State.Lazy
 
+import Test.QuickCheck
+
 -- A type for CNF: a list (conjunction) of lists (clauses) of
 -- literas (positive or negative variable names)
 type CNF = [[Var]]
 
+toCNF' :: Var -> Formula.F -> (Var, CNF, Var)
+toCNF' next f = case f of
+  Var n -> (n, [], next)
+  Neg g ->
+    let (vg, cs, vf) = toCNF' next g in
+      (vf, cs ++ [[-vg, -vf], [vg, vf]], vf + 1)
+  g :/\: h ->
+    rec2 g h (\vg vh vf ->
+      [[-vg, -vh, vf], [vg, -vf], [vh, -vf]])
+  g :\/: h ->
+    rec2 g h (\vg vh vf ->
+      [[vg, vh, -vf], [-vg, vf], [-vh, vf]])
+  g :<=>: h ->
+    rec2 g h (\vg vh vf ->
+      [[-vg, -vh, vf], [vg, vh, vf], [vg, -vh, -vf], [-vg, vh, -vf]])
+  g :=>: h ->
+    rec2 g h (\vg vh vf ->
+      [[-vg, vh, -vf], [vg, vf], [-vh, vf]])
+  where
+    rec2 g h gen =
+      let (vg, csF, n1) = toCNF' next g
+          (vh, csG, vf) = toCNF' n1 h in
+        (vf, csF ++ csG ++ gen vg vh vf, vf + 1)
+
+
 -- Tseitin's conversion
-toCNF f = undefined
+toCNF :: Formula.F -> CNF
+toCNF (Var n) = [[n]]
+toCNF f =
+  let (top, cs, _) = toCNF' (maxVar f + 1) f in
+    cs ++ [[top]]
+
 
 -- The inverse conversion
+fromCNF :: CNF -> Formula.F
 fromCNF (h : tl) = foldl (\ acc c -> acc :/\: fromClause c) (fromClause h) tl where
   fromClause (h : tl)  = foldl (\ acc v -> acc :\/: fromVar v) (fromVar h) tl
   fromVar n | n < 0     = Neg $ Var (-n)
@@ -27,4 +60,4 @@ equisaT f = Formula.equisat f (fromCNF $ CNF.toCNF f)
 
 -- Tseitin's conversion gives a CNF
 isCNF :: F -> Bool
-isCNF = Formula.isCNF . fromCNF . CNF.toCNF 
+isCNF = Formula.isCNF . fromCNF . CNF.toCNF
