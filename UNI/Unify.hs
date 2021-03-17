50b8d00290d2f3573b464c74751c65153abc7278
Your Name <you@example.com>
2021-03-17 23:02:34 +0300

    Finish hw04

--- UNI/Unify.hs
+++ UNI/Unify.hs
@@ -8,23 +8,35 @@ import Control.Monad
 import Data.List
 import qualified Term as T
 import qualified Test.QuickCheck as QC
+import Util (farthestDNC)
 
 -- Walk primitive: given a variable, lookups for the first
 -- non-variable binding; given a non-variable term returns
 -- this term
 walk :: T.Subst -> T.T -> T.T
-walk = undefined
+walk s = farthestDNC (\t -> case t of
+  T.V _ -> T.apply1 s t
+  T.C _ _ -> t)
 
 -- Occurs-check for terms: return true, if
 -- a variable occurs in the term
 occurs :: T.Var -> T.T -> Bool
-occurs = undefined
+occurs v t = v `elem` T.fv t
 
 -- Unification generic function. Takes an optional
 -- substitution and two terms and returns an optional
 -- MGU
 unify :: Maybe T.Subst -> T.T -> T.T -> Maybe T.Subst
-unify = undefined
+unify ms t1 t2 = case ms of
+  Nothing -> Nothing
+  Just s -> case (walk s t1, walk s t2) of
+    (T.V v1, T.V v2) | v1 == v2 -> ms
+    (T.V v, t) -> Just (s T.<+> T.add T.empty v t)
+    (t, T.V v) -> Just (s T.<+> T.add T.empty v t)
+    (T.C c1 ts1, T.C c2 ts2) | c1 == c2 && length ts1 == length ts2 ->
+      foldr (\(t1, t2) s -> unify s t1 t2) ms $ zip ts1 ts2
+    _ -> Nothing
+
 
 -- An infix version of unification
 -- with empty substitution
