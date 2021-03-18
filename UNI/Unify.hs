b0ffb265819ab7668fc0e7997d52e2c95da14cb0
crossopt <s17b2_fedorkina@179.ru>
2021-03-18 17:35:51 +0300

    unify done

--- UNI/Unify.hs
+++ UNI/Unify.hs
@@ -27,7 +27,15 @@ occurs v t = elem v (T.fv t)
 -- substitution and two terms and returns an optional
 -- MGU
 unify :: Maybe T.Subst -> T.T -> T.T -> Maybe T.Subst
-unify = undefined
+unify s t1 t2 = s >>= (\s' ->
+    case (walk s' t1, walk s' t2) of
+        (T.V v1, T.V v2) | (v1 == v2) -> s
+        (T.V v, t) | not (occurs v t) -> Just (T.add s' v t)
+        (t, T.V v) | not (occurs v t) -> Just (T.add s' v t)
+        (T.C c1 t1', T.C c2 t2') | (c1 == c2 && length t1' == length t2')
+            -> foldl (\x (p, q) -> unify x p q) s $ zip t1' t2'
+        _ -> Nothing
+    )
 
 -- An infix version of unification
 -- with empty substitution
