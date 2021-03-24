7f68c8b3dcbb2508fc511aab6f4f2e460f34a7d0
Ilya Kaysin <demarkok@gmail.com>
2021-03-24 14:58:27 +0300

    Add another version

--- Miss-n-Cann/MissNCann.ml
+++ Miss-n-Cann/MissNCann.ml
@@ -8,14 +8,22 @@ open OCanren.Std
 @type boat  = Left | Right with show
 @type lboat = ocanren {boat} with show
 
+(*
+   The state of the rivebank is defined by a tuple: Miss, Cann
+   where Miss - number of Missioners on the bank
+         Cann - number of Cannibals on the bank
+*)
+@type bank = int * int with show
+@type lbank = ocanren {int * int} with show
 
-(* The current state is defined by a triple: Miss, Cann, and Boat
-   where Miss - number of Mission on the _current_ bank (where the boat is located)
-         Cann - same for Cannibals
+
+(* The current state is defined by a triple: LBank, RBank, and Boat
+   where LBank - the state of the Left bank
+         RBank - the state of the Right bank
          Boat - the position of the boat
 *)
-@type state = int * int * boat with show
-@type lstate = ocanren {int * int * boat} with show
+@type state = bank * bank * boat with show
+@type lstate = ocanren {bank * bank * boat} with show
 
 (* The move is a pair DMiss and DCann
    where DMiss - the number of Missioners embarking on the boat
@@ -38,58 +46,59 @@ let right () = inj @@ lift Right
 *)
 let sound move state =
   Nat.(ocanren {
-    fresh dMiss, dCann, miss, cann, boat in
+    fresh dMiss, dCann, missL, cannL, missR, cannR, boat in
     move == (dMiss, dCann) &
-    state == (miss, cann, boat) &
+    state == ((missL, cannL), (missR, cannR), boat) &
     fresh tot in (+) dMiss dCann tot & tot <= 2 & tot > 0 &
-    dMiss <= miss & dCann <= cann }
-  )
+    {
+      {boat == Left  & dMiss <= missL & dCann <= cannL}
+    | {boat == Right & dMiss <= missR & dCann <= cannR}
+    }
+  })
 
 (* The number of Cannibals must not exceed the number of Missioners on the either bank *)
 let safe state =
   Nat.(ocanren {
-    fresh miss, cann,boat in
-    state == (miss, cann, boat) &
+    fresh missL, cannL, missR, cannR, boat in
+    state == ((missL, cannL), (missR, cannR), boat) &
     {
-      miss == 0
-    | miss >= cann
+      missL == 0
+    | missL >= cannL
     } &
     {
-      miss == 2
-    | miss <= cann
+      missR == 0
+    | missR >= cannR
     }
   })
 
 (* Perform one step *)
 let step move state state' =
-  let step = Tabling.tabledrec Tabling.three (fun step move state state' ->
+  let step = Tabling.tabledrec Tabling.three (fun step move state state' -> 
   Nat.(ocanren {
     fresh dMiss, dCann in
-    fresh miss, cann, boat in
-    fresh miss', cann', boat' in
+    fresh missL, cannL, missR, cannR, boat in
+    fresh missL', cannL', missR', cannR', boat' in
+
     move == (dMiss, dCann) &
-    state == (miss, cann, boat) &
-    state' == (miss', cann', boat') &
+    state == ((missL, cannL), (missR, cannR), boat) &
+    state' == ((missL', cannL'), (missR', cannR'), boat') &
 
     sound move state &
     safe state' &
-    {
-      boat == Left & boat' == Right
-    | boat == Right & boat' == Left
-    } &
-    (* miss' == n - miss + dMiss *)
-    fresh m1, m2 in
-    (+) miss miss' m1 &
-    (+) 2 dMiss m2 &
-    m1 == m2 &
+    { {boat == Left & boat' == Right &
+       fresh sM1 in (+) missR dMiss sM1 & sM1 == missR' &
+       fresh sM2 in (+) missL' dMiss sM2 & sM2 == missL &
 
-    (* cann' == n - cann + dCann *)
-    fresh c1, c2 in
-    (+) cann cann' c1 &
-    (+) 2 dCann c2 &
-    c1 == c2
+       fresh sC1 in (+) cannR dCann sC1 & sC1 == cannR' &
+       fresh sC2 in (+) cannL' dCann sC2 & sC2 == cannL}
+    | {boat == Right & boat' == Left &
+       fresh sM1 in (+) missR' dMiss sM1 & sM1 == missR &
+       fresh sM2 in (+) missL dMiss sM2 & sM2 == missL' &
+
+       fresh sC1 in (+) cannR' dCann sC1 & sC1 == cannR &
+       fresh sC2 in (+) cannL dCann sC2 & sC2 == cannL'}
+    }
   }))
-
 in step move state state'
 
 
@@ -108,4 +117,4 @@ let _ =
   show(List.ground) (show(Pair.ground) (show(OCanren.Std.Nat.ground)) (show(OCanren.Std.Nat.ground)))  @@
   L.hd @@
   Stream.take ~n:1 @@
-  run q (fun q -> ocanren {eval q (2, 2, Left) (2, 2, Right)}) project
+  run q (fun q -> ocanren {eval q ((2, 2), (0, 0), Left) ((0, 0), (2, 2), Right)}) project
