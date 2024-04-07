77536e1d6c57657f2cc3ce9f2fc20e4079a54718
blonded04 <generalretcher@gmail.com>
2024-04-08 01:21:22 +0300

    well, its type-checked but im pretty sure it doesn't work so just wait for the tests D:

--- SLD/SLD.hs
+++ SLD/SLD.hs
@@ -1,5 +1,6 @@
 import Term as T
 import Unify as U
+import Data.Map as Map
 
 -- Predicate names
 type Pred = Int
@@ -31,13 +32,40 @@ type C = (P, [(P, T.Subst, [A])])
 eval :: P -> [A] -> [T.Subst]
 eval p q = evalRec (p, [(p, T.empty, q)])
 
--- Rename a program so that all variables in it are not 
--- interlapping with a given substitution: takes
+-- Rename a horn clause so that all variables in it are 
+-- not interlapping with a given substitution: takes
 --   1. a substitution
 --   2. source program
---   3. part of the program that needs to be renamed
-renameProgramHelper :: T.Subst -> P -> P -> P
-renameProgramHelper = undefined
+--   3. horn clause that needs to be renamed
+renameHornHelper :: T.Subst -> P -> H -> H
+renameHornHelper s p = renameHornWith (getRenamingShift s p) where
+  getRenamingShift :: T.Subst -> P -> Int 
+  getRenamingShift s p = if T.empty == s then getProgramShift p else max (getProgramShift p) (fst $ Map.findMax s)
+  getProgramShift :: P -> Int
+  getProgramShift []              = 0
+  getProgramShift ((ts, as):rest) = max (getMaxIndex (ts : as)) (getProgramShift rest)
+  getMinIndex :: [A] -> Int
+  getMinIndex [(_, ts)]      = minimum (getMinInd <$> ts)
+  getMinIndex ((_, ts):rest) = min (minimum (getMinInd <$> ts)) (getMinIndex rest)
+  getMinInd :: T.T -> Int
+  getMinInd (C _ ts) = minimum (getMinInd <$> ts)
+  getMinInd (V var)  = var
+  getMaxIndex :: [A] -> Int
+  getMaxIndex [(_, ts)]      = maximum (getMaxInd <$> ts)
+  getMaxIndex ((_, ts):rest) = max (maximum (getMaxInd <$> ts)) (getMaxIndex rest)
+  getMaxInd :: T.T -> Int
+  getMaxInd (C _ ts) = maximum (getMaxInd <$> ts)
+  getMaxInd (V var)  = var
+  renameHornWith :: Int -> H -> H
+  renameHornWith startsFrom (ts, as) = let res = renameHornWithHelper startsFrom (getMinIndex (ts : as) + 1) (ts : as) in
+                                       (head res, tail res)
+  renameHornWithHelper :: Int -> Int -> [A] -> [A]
+  renameHornWithHelper startsFrom delta []             = []
+  renameHornWithHelper startsFrom delta ((a, ts):rest) = (a, renameTermWith startsFrom delta <$> ts)
+                                                         : renameHornWithHelper startsFrom delta rest
+  renameTermWith :: Int -> Int -> T.T -> T.T
+  renameTermWith startsFrom delta (V var)  = V (var - startsFrom + delta)
+  renameTermWith startsFrom delta (C c ts) = C c (renameTermWith startsFrom delta <$> ts)
 
 -- Recursive evalutor
 evalRec :: C -> [T.Subst]
@@ -46,13 +74,12 @@ evalRec (p, (t, s, []) : tl)                        = s : evalRec (p, tl)
 evalRec (p, (t, s, goal@((atom, terms) : as)) : tl) = 
   case t of
     [] -> evalRec (p, tl)
-    ((newAtom, predTerms), predAtoms) : programTail -> 
+    h@((newAtom, predTerms), predAtoms) : programTail -> 
       if newAtom /= atom then 
         evalRec (p, (programTail, s, goal) : tl)
-      else let renamedT           = renameProgramHelper s p t in
-           let renamedPredAtoms   = snd (head renamedT)       in
-           let renamedPredTerms   = snd (fst (head renamedT)) in
-           let renamedProgramTail = tail renamedT             in
+      else let renamedT           = renameHornHelper s p h in
+           let renamedPredAtoms   = snd renamedT           in
+           let renamedPredTerms   = snd (fst renamedT)     in
            case unifyLists (Just s) renamedPredTerms terms of
              Nothing  -> evalRec (p, (programTail, s, goal) : tl)
              Just mgu -> evalRec (p, (p, mgu, renamedPredAtoms ++ as) : ((programTail, s, goal) : tl))
