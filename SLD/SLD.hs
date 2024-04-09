c56ea921a90aa6938b64d6773d0a4241ee46bb61
blonded04 <generalretcher@gmail.com>
2024-04-09 14:12:45 +0300

    small bug fixed

--- SLD/SLD.hs
+++ SLD/SLD.hs
@@ -39,48 +39,53 @@ eval p q = evalRec (p, [(p, T.empty, q)])
 --   3. horn clause that needs to be renamed
 renameHornHelper :: T.Subst -> P -> H -> H
 renameHornHelper s p = renameHornWith (getRenamingShift s p) where
+  myInfTy :: Int
+  myInfTy = 2000000000
   getRenamingShift :: T.Subst -> P -> Int 
   getRenamingShift s p = if T.empty == s then getProgramShift p else max (getProgramShift p) (fst $ Map.findMax s)
   getProgramShift :: P -> Int
   getProgramShift []              = 0
   getProgramShift ((ts, as):rest) = max (getMaxIndex (ts : as)) (getProgramShift rest)
   getMinIndex :: [A] -> Int
-  getMinIndex [(_, ts)]      = minimum (getMinInd <$> ts)
+  getMinIndex []             = myInfTy
   getMinIndex ((_, ts):rest) = min (minimum (getMinInd <$> ts)) (getMinIndex rest)
   getMinInd :: T.T -> Int
+  getMinInd (C _ []) = myInfTy
   getMinInd (C _ ts) = minimum (getMinInd <$> ts)
   getMinInd (V var)  = var
   getMaxIndex :: [A] -> Int
-  getMaxIndex [(_, ts)]      = maximum (getMaxInd <$> ts)
+  getMaxIndex []             = 0
   getMaxIndex ((_, ts):rest) = max (maximum (getMaxInd <$> ts)) (getMaxIndex rest)
   getMaxInd :: T.T -> Int
+  getMaxInd (C _ []) = 0
   getMaxInd (C _ ts) = maximum (getMaxInd <$> ts)
   getMaxInd (V var)  = var
   renameHornWith :: Int -> H -> H
-  renameHornWith startsFrom (ts, as) = let res = renameHornWithHelper startsFrom (getMinIndex (ts : as) + 1) (ts : as) in
+  renameHornWith startsFrom (ts, as) = let res = renameHornWithHelper startsFrom (getMinIndex (ts : as)) (ts : as) in
                                        (head res, tail res)
   renameHornWithHelper :: Int -> Int -> [A] -> [A]
   renameHornWithHelper startsFrom delta []             = []
   renameHornWithHelper startsFrom delta ((a, ts):rest) = (a, renameTermWith startsFrom delta <$> ts)
                                                          : renameHornWithHelper startsFrom delta rest
   renameTermWith :: Int -> Int -> T.T -> T.T
-  renameTermWith startsFrom delta (V var)  = V (var - startsFrom + delta)
+  renameTermWith startsFrom delta (V var)  = V (var - delta + startsFrom + 1)
   renameTermWith startsFrom delta (C c ts) = C c (renameTermWith startsFrom delta <$> ts)
 
 -- Recursive evalutor
 evalRec :: C -> [T.Subst]
-evalRec (p, [(t, s, [])])                           = [s]
+evalRec (p, [])                                     = []
 evalRec (p, (t, s, []) : tl)                        = s : evalRec (p, tl)
 evalRec (p, (t, s, goal@((atom, terms) : as)) : tl) = 
   case t of
     [] -> evalRec (p, tl)
     h@((newAtom, predTerms), predAtoms) : programTail -> 
+      let newStack = (programTail, s, goal) : tl in
       if newAtom /= atom then 
-        evalRec (p, (programTail, s, goal) : tl)
+        evalRec (p, newStack)
       else let renamedT           = renameHornHelper s p h in
            let renamedPredAtoms   = snd renamedT           in
            let renamedPredTerms   = snd (fst renamedT)     in
            case unifyLists (Just s) renamedPredTerms terms of
-             Nothing  -> evalRec (p, (programTail, s, goal) : tl)
-             Just mgu -> evalRec (p, (p, mgu, renamedPredAtoms ++ as) : ((programTail, s, goal) : tl))
+             Nothing  -> evalRec (p, newStack)
+             Just mgu -> evalRec (p, (p, mgu, renamedPredAtoms ++ as) : newStack)
                                                 
